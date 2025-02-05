79638db0a1d9f3ead003a1dc389354207504203b
souvik666 <peacedynasty0@gmail.com>
2025-02-05 20:13:41 +0530

    [2025-02-05 20:13:41] 🔧 UPDATED: gic.sh

--- gic.sh
+++ gic.sh
@@ -22,7 +22,7 @@ else
   RESET=""
 fi
 
-# Default commit tags
+# Default commit tags (Plain text for commit message)
 MODIFIED_TAG="🔧 UPDATED"
 ADDED_TAG="✨ ADDED"
 DELETED_TAG="🗑️ REMOVED"
@@ -54,32 +54,56 @@ branch_name=$(git rev-parse --abbrev-ref HEAD)
 issue_number=$(echo "$branch_name" | grep -oE '[0-9]+')
 
 commit_message_lines=()
+commit_message_plain=()
+
 while read -r line; do
   status=$(echo $line | awk '{print $1}')
   file=$(echo $line | awk '{$1=""; print $0}' | xargs) 
 
   case $status in
-    M) commit_message_lines+=("${GREEN}$MODIFIED_TAG: $file${RESET}");;
-    A) commit_message_lines+=("${CYAN}$ADDED_TAG: $file${RESET}");;
-    D) commit_message_lines+=("${RED}$DELETED_TAG: $file${RESET}");;
-    ??) commit_message_lines+=("${BLUE}$NEW_TAG: $file${RESET}");;
-    *) commit_message_lines+=("${YELLOW}$OTHER_TAG: $file${RESET}");;
+    M) 
+      commit_message_lines+=("${GREEN}$MODIFIED_TAG: $file${RESET}")
+      commit_message_plain+=("$MODIFIED_TAG: $file")
+      ;;
+    A) 
+      commit_message_lines+=("${CYAN}$ADDED_TAG: $file${RESET}")
+      commit_message_plain+=("$ADDED_TAG: $file")
+      ;;
+    D) 
+      commit_message_lines+=("${RED}$DELETED_TAG: $file${RESET}")
+      commit_message_plain+=("$DELETED_TAG: $file")
+      ;;
+    ??) 
+      commit_message_lines+=("${BLUE}$NEW_TAG: $file${RESET}")
+      commit_message_plain+=("$NEW_TAG: $file")
+      ;;
+    *) 
+      commit_message_lines+=("${YELLOW}$OTHER_TAG: $file${RESET}")
+      commit_message_plain+=("$OTHER_TAG: $file")
+      ;;
   esac
 done <<< "$changes"
 
 date_time="$(date +'%Y-%m-%d %H:%M:%S')"
 
 if [ -n "$issue_number" ]; then
-  commit_message="(#$issue_number) [$date_time] ${commit_message_lines[0]}"
+  commit_message="(#$issue_number) [$date_time] ${commit_message_plain[0]}"
 else
-  commit_message="[$date_time] ${commit_message_lines[0]}"
+  commit_message="[$date_time] ${commit_message_plain[0]}"
 fi
 
-for ((i=1; i<${#commit_message_lines[@]}; i++)); do
-  commit_message+="\n${commit_message_lines[i]}"
+for ((i=1; i<${#commit_message_plain[@]}; i++)); do
+  commit_message+="\n${commit_message_plain[i]}"
 done
 
-echo -e "${GREEN}Committing changes...${RESET}"
+# Show colored commit preview but commit without colors
+echo -e "\n${GREEN}Commit Preview:${RESET}"
+for msg in "${commit_message_lines[@]}"; do
+  echo -e "$msg"
+done
+echo ""
+
+# Commit changes
 git commit -m "$(echo -e "$commit_message")"
 echo -e "${CYAN}Pushing to remote...${RESET}"
 git push
